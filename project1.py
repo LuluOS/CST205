9d69d4d247c92f1e4d8f120a42d534a18bfced04
Luana <lokinosawada@csumb.edu>
2015-09-17 17:36:23 -0700

    BnW
    add black and white filter

--- project1.py
+++ project1.py
@@ -43,26 +43,24 @@ def quicksort(list, start, end):
     return
 
 #""" Picking picture """
-def pickPicture(vector):
+def pickFolder(vector):
   folder = pickAFolder()
   if folder is not None:
-    photos = []
 
     for imageNumber in range(0,8):
       imagePath = folder + str(imageNumber + 1) + ".png"
-      photos.append(makePicture(imagePath))
-  
-    return photos
-  else:
-    print ("!!! ERROR: invalid input for pickPicture() !!!")
+      vector.append(makePicture(imagePath))
 
-#""" createNewPicture(xSize, ySize) """
-def createNewPicture(xSize, ySize, nameOfImage):
+    return folder
+  else:
+    print ("!!! ERROR: invalid input for pickFolder() !!!")
+
+#""" create the new picture """
+def createNewPicture(xSize, ySize, nameOfImage, folder):
   #""" creating a new picture with the size of xSize X ySize """
   newPicture = makeEmptyPicture(xSize, ySize)
   #""" defining a path for the new picture """
-  newPath = "C:\\Users\\Luana\\Desktop\\CSUMB\\CST205-Multimedia\\Project1\\Project1Images\\"
-  newPath = newPath + str(nameOfImage) + ".png"
+  newPath = folder + str(nameOfImage) + ".png"
 
   pictureDetails = []
   pictureDetails.append(newPicture) #""" pictureDetails[0] = newPicture """
@@ -73,7 +71,7 @@ def createNewPicture(xSize, ySize, nameOfImage):
 
   return pictureDetails
   
-#"""saveNewPicture(newPicture, newPath)"""
+#"""save the new picture """
 def saveNewPicture(newPicture, newPath):
   #""" saving the new picture in newPath """
   writePictureTo(newPicture, newPath)
@@ -81,6 +79,7 @@ def saveNewPicture(newPicture, newPath):
   show(newPicture)
   return
 
+#""" Analyze the pixels """
 def analyzePixel(px, pic, x, y, red, blue, green):
   #""" navegating in the same pixel to compare each pixel from the 9 images """
   for picture in range (0, len(pic)):
@@ -140,7 +139,7 @@ def mode(lst, pxSize):
 
   often = 0
   bigger = 0
-  flag = 0
+  goOn = 0
   quantity = []
   for i in range (len(numbers)):
     qnt = diffQuantity(numbers[i], lst)
@@ -148,14 +147,14 @@ def mode(lst, pxSize):
     if (bigger < quantity[i]):
       bigger = quantity[i]
       often = numbers[i]
-      flag = 1
+      goOn = 1
     else:
       if (bigger > quantity[i]):
-        flag = 2
+        goOn = 2
       else:
-        flag = 0
+        goOn = 0
 
-  if (flag == 0):
+  if (goOn == 0):
     return median(lst,pxSize)
   else:
     return often
@@ -168,23 +167,38 @@ def rangeArray(lst, pxSize):
 
   return rangeOf
 
-#""" return the range of the lst """ 
-def bnw(lst, pxSize):
-  quicksort(lst,0,pxSize)
-  m = median(lst,pxSize)
+def mean(lst):
+  total = 0
+  for i in range(len(lst)):
+    total = total + lst[i]
+  mean = total / len(lst)
+  return mean
 
-  #print ("M = %d" %m)
-  if (m >= 0) & (m <= 127):
-    m = 0
+def approximateBnW(color):
+  if (color >= 0) & (color <= 127): #""" if the color is near to black than white """
+    color = 0 #""" color turn black """
   else:
-      if (m >= 128) & (m <= 255):
-        m = 255
-     
-  return m
+      if (color > 128) & (color <= 255): #""" if the color is near to white than black """
+        color = 255 #""" color turn white """
+  return color
+
+#""" return the range of the lst """ 
+def bnw(Red,Green,Blue):
+  average = (Red + Green + Blue) / 3
+  color = approximateBnW(average)
+
+  vector = []
+  Red = color
+  Green = color
+  Blue = color
+  vector.append(Red) #""" vector[0] = Red """
+  vector.append(Green) #""" vector[1] = Green """
+  vector.append(Blue) #""" vector[2] = Blue """
+  return vector
 
 #""" Filtering with the median """
-def medianFilter(pic, ySize, xSize):
-  pictureDetails = createNewPicture(xSize, ySize, "medianFilter")
+def medianFilter(pic, ySize, xSize, folder):
+  pictureDetails = createNewPicture(xSize, ySize, "MedianFilter", folder)
 
   #"""Navigating in y-axes """
   for y in range (0, ySize):
@@ -220,8 +234,8 @@ def medianFilter(pic, ySize, xSize):
   return
 
 #""" Filtering with the mode with the number which appears most """
-def modeFilter(pic, ySize, xSize):
-  pictureDetails = createNewPicture(xSize, ySize, "modeFilter")
+def modeFilter(pic, ySize, xSize, folder):
+  pictureDetails = createNewPicture(xSize, ySize, "ModeFilter", folder)
 
   #"""Navigating in y-axes """
   for y in range (0, ySize):
@@ -257,8 +271,8 @@ def modeFilter(pic, ySize, xSize):
   return
 
 #""" Filtering with the range """
-def rangeFilter(pic, ySize, xSize):
-  pictureDetails = createNewPicture(xSize, ySize, "rangeFilter")
+def rangeFilter(pic, ySize, xSize, folder):
+  pictureDetails = createNewPicture(xSize, ySize, "RangeFilter", folder)
 
   #"""Navigating in y-axes """
   for y in range (0, ySize):
@@ -293,9 +307,8 @@ def rangeFilter(pic, ySize, xSize):
 
   return
 
-#""" Filtering with the range """
-def BnWFilter(pic, ySize, xSize):
-  pictureDetails = createNewPicture(xSize, ySize, "rangeFilter")
+def meanFilter(pic, ySize, xSize, folder):
+  pictureDetails = createNewPicture(xSize, ySize, "MeanFilter", folder)
 
   #"""Navigating in y-axes """
   for y in range (0, ySize):
@@ -312,12 +325,52 @@ def BnWFilter(pic, ySize, xSize):
       npx = getPixel(pictureDetails[0], x, y)
       pxSize = len(px)-1
       
-      mRed = bnw(red, pxSize)
-      mGreen = bnw(green, pxSize)
-      mBlue = bnw(blue, pxSize)
+      mRed = mean(red)
+      mGreen = mean(green)
+      mBlue = mean(blue)
 
       #""" put all RGB color together """
       color = makeColor(mRed, mGreen, mBlue)
+      #""" set pixel npx with the color """
+      setColor(npx, color)
+
+      #""" deleting the px array """
+      del px
+
+    repaint(pictureDetails[0])
+
+  saveNewPicture(pictureDetails[0], pictureDetails[1])
+
+  return  
+
+#""" Black and White Filtering """
+def BnWFilter(pic, ySize, xSize, folder):
+  pictureDetails = createNewPicture(xSize, ySize, "BnWFilter", folder)
+
+  #"""Navigating in y-axes """
+  for y in range (0, ySize):
+    #"""Navigating in x-axes """
+    for x in range (0, xSize):
+      #""" declare a new vectors """
+      red = []
+      blue = []
+      green = []
+      px = []
+      bnwColor = []
+
+      analyzePixel(px, pic, x, y, red, blue, green)
+      
+      npx = getPixel(pictureDetails[0], x, y)
+      pxSize = len(px)-1
+      
+      mRed = median(red, pxSize)
+      mGreen = median(green, pxSize)
+      mBlue = median(blue, pxSize)
+
+      bnwColor = bnw(mRed,mGreen,mBlue)
+
+      #""" put all RGB color together """
+      color = makeColor(bnwColor[0], bnwColor[1], bnwColor[2])
       #""" set pixel npx with the color """
       setColor(npx, color)
 
@@ -336,9 +389,10 @@ def iniciate():
 
   #""" declare a new array from picture """
   pic = []
-  #""" add the pictures in the end of the array """
-  pic = pickPicture(pic)
-
+  
+  #""" pick all the photos and return the path of the folder """
+  folder = pickFolder(pic)
+  
   #""" showing all the 9 pictures """
   #for i in range (0, len(pic)):
   # show(pic[i])
@@ -351,23 +405,45 @@ def iniciate():
     configuration.append(pic) #""" configuration[0] = pic """
     configuration.append(xSize) #""" configuration[1] = xSize """
     configuration.append(ySize) #""" configuration[2] = ySize """
+    configuration.append(folder) #""" configuration[3] = folder """
   
   return configuration
   
 #""" Main """
+again = True
 configuration = iniciate()
-if (configuration): #""" check if the array is different from null """
-  number = requestNumber("Choose a filter: 1-MedianFilter, 2-ModeFilter, 3-RangeFilter, 4-BlackNWhiteFilter")
-  if (number == 1):
-    medianFilter(configuration[0], configuration[2], configuration[1])
-  else: 
-    if (number == 2):
-      modeFilter(configuration[0], configuration[2], configuration[1])
-    else:
-      if (number == 3):
-        rangeFilter(configuration[0], configuration[2], configuration[1])
-      else:
-        if (number == 4):
-          BnWFilter(configuration[0], configuration[2], configuration[1])
-else:
-  print ("!!! ERROR: couldn't iniciate !!!")
+while (again == True):
+  
+  goOn = True
+  if (configuration): #""" check if the array is different from null """
+    while (goOn == True): #""" If the user select a number that is not accept as a input everything above is gonna repeat """
+      number = requestInteger("Choose a filter:\n 1-MedianFilter, 2-ModeFilter,\n 3-RangeFilter, 4-MeanFilter,\n 5-BlackNWhiteFilter")
+      if (number == 1):
+        goOn = False
+        medianFilter(configuration[0], configuration[2], configuration[1], configuration[3])
+      else: 
+        if (number == 2):
+          goOn = False
+          modeFilter(configuration[0], configuration[2], configuration[1], configuration[3])
+        else:
+          if (number == 3):
+            goOn = False
+            rangeFilter(configuration[0], configuration[2], configuration[1], configuration[3])
+          else:
+            if (number == 4):
+              goOn = False
+              meanFilter(configuration[0], configuration[2], configuration[1], configuration[3])
+            else:
+              if (number == 5):
+                goOn = False
+                BnWFilter(configuration[0], configuration[2], configuration[1], configuration[3])
+              else:
+                goOn = True
+  else:
+    print ("!!! ERROR: couldn't iniciate !!!")
+
+  confirmation = requestInteger("Do you wanna create another filter for the same images? \n1-Yes 2-No")
+  if (confirmation == 1):
+    again = True
+  else:
+    again = False
